f15b6a21bea6024f9b3d89b8a7db45dc5dfef0fe
RasikaKhandare <67459684+RasikaKhandare@users.noreply.github.com>
2020-07-03 23:06:27 +0530

    Document Details work

---
+++ src/main/java/com/webo/app/twowheelerloan/loanprocess/main/service/HomeServiceDocumentDetails.java
@@ -0,0 +1,8 @@
+package com.webo.app.twowheelerloan.loanprocess.main.service;
+
+import com.webo.app.twowheelerloan.loanprocess.main.model.DocumentDetails;
+
+public interface HomeServiceDocumentDetails {
+	public void savedata(DocumentDetails doc);
+
+}
